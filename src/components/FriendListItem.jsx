9cfa13bae7a1932d6593dfd145a63578055940d2
Oleksandra Zakharova <oleksandraozakharova@gmail.com>
2025-01-22 14:15:31 +0100

    fixed color issues in task 2

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import css from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
         <div>
             <img src={avatar} alt="Avatar" width="48" />
             <p>{name}</p>
-            <p>{isOnline ? 'Online' : 'Offline'}</p>
+            <p
+        className={clsx(
+          css.friendStatus,
+          isOnline && css.isOnline,
+          !isOnline && css.isOffline
+        )}
+      >
+        {isOnline ? 'Online' : 'Offline'}
+      </p>
         </div>
 
     );
